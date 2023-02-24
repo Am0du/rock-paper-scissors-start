ca968112a38cf3b500d369ce3fb355e54b87aa3c
Amodu David <104693505+Am0du@users.noreply.github.com>
2023-02-24 01:46:55 +0000

    Stopped the game from running if an invalid input was given by the user

--- main.py
+++ main.py
@@ -32,34 +32,33 @@ game = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
 
-#prints out the user's choice
-if user_choice >=0 and user_choice <= 2:
-  print(game[user_choice])
-else:
-  print("invalid input")
-  
-#generates a random number for the computer
-print("Computer chose:")
-com_choice = random.randint(0, 2)
-
-#Outputs the choice of the computer
-print(game[com_choice])
-  
-
-#Selects winner
-if user_choice == 0 and com_choice == 1:
-  print("you lost, paper wins rock")
-elif user_choice == 1 and com_choice == 0:
-  print("You win, Paper wins rock")
-elif user_choice == 2 and com_choice == 1:
-  print("You lose, Scissor wins paper")
-elif user_choice == 1 and com_choice == 2:
-  print("You win, Scissor wins paper ")
-elif user_choice == 2 and com_choice == 0:
-  print("You lose, Rock wins scissors")
-elif user_choice == 0 and com_choice == 2:
-  print("You win, Rock wins scissors ")
-elif user_choice >= 3 or user_choice < 0:
+if user_choice >= 3 or user_choice < 0:
   print("Invalid number, You lose")
 else:
-  print("its a draw")
+#prints out the user's choice
+  print(game[user_choice])
+
+#generates a random number for the computer
+  print("Computer chose:")
+  com_choice = random.randint(0, 2)
+  
+  #Outputs the choice of the computer
+  print(game[com_choice])
+    
+  
+  #Selects winner
+  if user_choice == 0 and com_choice == 1:
+    print("you lost, paper wins rock")
+  elif user_choice == 1 and com_choice == 0:
+    print("You win, Paper wins rock")
+  elif user_choice == 2 and com_choice == 1:
+    print("You lose, Scissor wins paper")
+  elif user_choice == 1 and com_choice == 2:
+    print("You win, Scissor wins paper ")
+  elif user_choice == 2 and com_choice == 0:
+    print("You lose, Rock wins scissors")
+  elif user_choice == 0 and com_choice == 2:
+    print("You win, Rock wins scissors ")
+  else:
+    print("its a draw")
+  
